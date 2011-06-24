e5f81268184c68a30fe85d3676283c16fd354a10
Giovanni Pellerano <evilaliv3@remotehost>
2011-06-24 09:37:48 +0200

    some code reduction.

--- src/janus.h
+++ src/janus.h
@@ -24,7 +24,6 @@
 
 #define CONST_JANUS_VERSION         "0.1"
 #define CONST_JANUS_IFNAME          "janus"
-#define CONST_JANUS_FAKEGW_IP       "212.77.1."
 #define CONST_JANUS_NETIP           "0.0.0.0"
 #define CONST_JANUS_NETMASK         "0.0.0.0"
 #define CONST_JANUS_LISTEN_IP       "127.0.0.1"
